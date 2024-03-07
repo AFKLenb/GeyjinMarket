5317951aee8295b9e2adcffbfc865c8a868acb58
AFKLenb <markermma@gmail.com>
2024-03-07 13:13:32 +0300

    Проект готов на 99%

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,12 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-               <h1 class="font-black uppercase text-[80px] text-center">Кто прочитал, тот ...</h1>
+            <div class="bg-white  overflow-hidden shadow-xl sm:rounded-lg">
+                <h3 class="text-[32px] mb-[80px] uppercase font-bold text-center">Happy customers</h3>
+                <div class="flex justify-around">
+                    <img src="{{ asset('/assets/gifs/1.gif') }}" class="w-[200px] h-[200px]" alt="">
+                    <img src="{{ asset('/assets/gifs/2.gif') }}" class="w-[200px] h-[200px]" alt="">
+                    <img src="{{ asset('/assets/gifs/3.gif') }}" class="w-[200px] h-[200px]" alt=""></div>
             </div>
         </div>
     </div>
